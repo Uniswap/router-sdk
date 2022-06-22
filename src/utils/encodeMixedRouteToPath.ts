f81420bdab76b06febf304dad31ea24a0cb6d069
Eric Zhong <eric.zhong@uniswap.org>
2022-06-22 12:16:53 -0400

    add encodeMixedRoutetoPath tests

--- src/utils/encodeMixedRouteToPath.ts
+++ src/utils/encodeMixedRouteToPath.ts
@@ -4,9 +4,12 @@ import { Pool } from '@uniswap/v3-sdk'
 import { Pair } from '@uniswap/v2-sdk'
 import { MixedRoute } from '../entities/mixedRoute'
 
+/// @dev We should import this from somewhere
+const V2_FEE = 8388608
+
 /**
  * Converts a route to a hex encoded path
- * @param route the v3 path to convert to an encoded path
+ * @param route the mixed path to convert to an encoded path
  * @param exactOutput whether the route should be encoded in reverse, for making exact output swaps
  */
 export function encodeMixedRouteToPath(route: MixedRoute<Currency, Currency>, exactOutput: boolean): string {
@@ -23,13 +26,13 @@ export function encodeMixedRouteToPath(route: MixedRoute<Currency, Currency>, ex
         return {
           inputToken: outputToken,
           types: ['address', 'uint24', 'address'],
-          path: [inputToken.address, part instanceof Pool ? part.fee : 0, outputToken.address],
+          path: [inputToken.address, part instanceof Pool ? part.fee : V2_FEE, outputToken.address],
         }
       } else {
         return {
           inputToken: outputToken,
           types: [...types, 'uint24', 'address'],
-          path: [...path, part instanceof Pool ? part.fee : 0, outputToken.address],
+          path: [...path, part instanceof Pool ? part.fee : V2_FEE, outputToken.address],
         }
       }
     },
